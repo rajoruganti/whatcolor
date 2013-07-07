773ea7bfb75d6da22cbf56550b421a3f275524e8
Raj Oruganti <raj@indusnet.co.in>
2013-07-07 08:47:15 +0530

    use correct node version

--- .openshift/cron/minutely/computecolor.sh
+++ .openshift/cron/minutely/computecolor.sh
@@ -2,5 +2,5 @@
 export LD_LIBRARY_PATH=$OPENSHIFT_DATA_DIR/usr/local/lib
 MIN=`date +%M`
 if [ $((${MIN#0} % 1)) == 0 ]; then
-    /usr/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
+    $OPENSHIFT_DATA_DIR/node-v0.9.1-linux-x64/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
 fi
