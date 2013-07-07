78858ad745d56eabe415e343bff44c75270549f9
Raj Oruganti <raj@indusnet.co.in>
2013-07-07 08:22:49 +0530

    get minute modulus in cron script

--- .openshift/cron/minutely/computecolor.sh
+++ .openshift/cron/minutely/computecolor.sh
@@ -1,4 +1,5 @@
-if [$(($(date +%H) % 12)) == 0 ]; then
-	# run the script
-	/usr/local/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
+#!/bin/bash
+MIN=`date +%M`
+if [ $((${MIN#0} % 10)) == 0 ]; then
+    /usr/local/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
 fi
