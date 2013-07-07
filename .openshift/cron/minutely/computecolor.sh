35fb51435c4227e8698b63343c552e0b5547e729
Raj Oruganti <raj@indusnet.co.in>
2013-07-07 08:52:58 +0530

    run compute every 5 monutes

--- .openshift/cron/minutely/computecolor.sh
+++ .openshift/cron/minutely/computecolor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$OPENSHIFT_DATA_DIR/usr/local/lib
 MIN=`date +%M`
-if [ $((${MIN#0} % 1)) == 0 ]; then
+if [ $((${MIN#0} % 5)) == 0 ]; then
     $OPENSHIFT_DATA_DIR/node-v0.9.1-linux-x64/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
 fi
