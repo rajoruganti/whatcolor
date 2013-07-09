932b18ad9673522894f3e2798e477a127f1f57b0
Raj Oruganti <raj@indusnet.co.in>
2013-07-09 16:23:06 +0530

    node_env in compute

--- .openshift/cron/minutely/computecolor.sh
+++ .openshift/cron/minutely/computecolor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$OPENSHIFT_DATA_DIR/usr/local/lib
+export NODE_ENV="production"
 MIN=`date +%M`
 if [ $((${MIN#0} % 10)) == 0 ]; then
     $OPENSHIFT_DATA_DIR/node-v0.9.1-linux-x64/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
