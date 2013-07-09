aa54bbbbc1463008610f85efb7f790535cf35ed1
Raj Oruganti <raj@indusnet.co.in>
2013-07-09 18:04:13 +0530

    just to push

--- .openshift/cron/minutely/computecolor.sh
+++ .openshift/cron/minutely/computecolor.sh
@@ -3,6 +3,7 @@ export LD_LIBRARY_PATH=$OPENSHIFT_DATA_DIR/usr/local/lib
 export NODE_ENV="production"
 MIN=`date +%M`
 if [ $((${MIN#0} % 10)) == 0 ]; then
+    echo "computing"
     $OPENSHIFT_DATA_DIR/node-v0.9.1-linux-x64/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
 else
     echo "not computing"
