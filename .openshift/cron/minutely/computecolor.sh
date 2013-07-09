6b038c08c528af11e43738ef132f840a9a92fc99
Raj Oruganti <raj@indusnet.co.in>
2013-07-09 13:42:51 +0530

    run compute every 10 minutes

--- .openshift/cron/minutely/computecolor.sh
+++ .openshift/cron/minutely/computecolor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$OPENSHIFT_DATA_DIR/usr/local/lib
 MIN=`date +%M`
-if [ $((${MIN#0} % 5)) == 0 ]; then
+if [ $((${MIN#0} % 10)) == 0 ]; then
     $OPENSHIFT_DATA_DIR/node-v0.9.1-linux-x64/bin/node $OPENSHIFT_REPO_DIR/computecolor.js
 else
     echo "not computing"
